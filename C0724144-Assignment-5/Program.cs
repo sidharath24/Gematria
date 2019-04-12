9ab3dc7775321b095ddef3a93a2b1f0dc834b187
Sidharath <sid.aggarwal.31@gmail.com>
2019-04-12 17:59:19 -0400

    Working code

--- C0724144-Assignment-5/Program.cs
+++ C0724144-Assignment-5/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace C0724144_Assignment_5
 {
-    class Program
+    class Program2
     {
-        static void Main(string[] args)
+        string[] Letters = new string[26] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+        public static void Main()
         {
+
+            Program2 orange = new Program2();
+            Console.WriteLine(orange.Gematria("universe"));
+            Console.ReadLine();
+        }
+        public int Gematria(string word)
+        {
+            int GematriaValue = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                GematriaValue += LetterValue(word[i].ToString());
+            }
+
+            return GematriaValue;
+        }
+        public int LetterValue(string letter)
+        {
+            int x = 0;
+            while (Letters[x] != letter)
+            {
+                if (Letters[x++] == letter)
+                return -1;
+            }
+            return x+1;
         }
     }
 }
